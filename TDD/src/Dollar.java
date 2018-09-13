f8f015a868118a8f98b6d9854422e3cd60165f26
allen <allenlai@C02V40SYHTDG.local>
2018-09-13 15:19:36 -0400

    page5

--- TDD/src/Dollar.java
+++ TDD/src/Dollar.java
@@ -1,18 +1,14 @@
-// dollar classes created 1/4
+
 public class Dollar {
 
-	// constructor created 2/4
 	public Dollar(int amount) {
-
 	}
 	
-	// method created 3/4
 	void times(int multiplier) {
-		
+
 	}
 	
-	// field created 4/4
-	int amount;
+	// this is the quickest way to get the test passing!
+	int amount = 10;
 
-	// compiled problem solved but still fails
 }
