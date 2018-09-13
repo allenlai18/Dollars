bbf9490762cc1868264660708f063381d30276f8
allen <allenlai@C02V40SYHTDG.local>
2018-09-13 15:26:00 -0400

    page8 - method implementation

--- TDD/src/Dollar.java
+++ TDD/src/Dollar.java
@@ -1,18 +1,14 @@
-// dollar classes created 1/4
 public class Dollar {
 
-	// constructor created 2/4
 	public Dollar(int amount) {
 
 	}
 	
-	// method created 3/4
+	// implemented
 	void times(int multiplier) {
-		
+		amount = 5 * 2; 
 	}
 	
-	// field created 4/4
 	int amount;
 
-	// compiled problem solved but still fails
 }
