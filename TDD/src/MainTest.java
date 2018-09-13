07a6ba9d5f47d543f61d3d5e900bd56f75584598
allen <allenlai@C02V40SYHTDG.local>
2018-09-13 13:50:28 -0400

    start

---
+++ TDD/src/MainTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+	    String str = "Junit is working fine";
+	      assertEquals("Junit is working fine",str);
+	
+	
+	}
+
+}
