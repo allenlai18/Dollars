95bde4cb66a65964a53cd789a421e5b4b74ea907
allen <allenlai@C02V40SYHTDG.local>
2018-09-13 15:22:41 -0400

    page5 - initializers

--- TDD/src/Dollar.java
+++ TDD/src/Dollar.java
@@ -1,14 +1,18 @@
-
+// class created
 public class Dollar {
 
+	// constructor created 2/4
 	public Dollar(int amount) {
+
 	}
 	
+	// method created 3/4
 	void times(int multiplier) {
-
+		
 	}
 	
-	// this is the quickest way to get the test passing!
-	int amount = 10;
+	// field created 4/4
+	int amount;
 
+	// compiled problem solved but still fails
 }
