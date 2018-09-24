a75b9dd3f138e1a8bb67d39b5b07ec9ff0150162
allen <allenlai@C02V40SYHTDG.fios-router.home>
2018-09-24 14:55:21 -0400

    cleaned up

--- TDD/src/MainTest.java
+++ TDD/src/MainTest.java
@@ -4,14 +4,6 @@ import org.junit.Test;
 
 public class MainTest {
 
-	@Test
-	public void test() {
-	    String str = "Junit is working fine";
-	      assertEquals("Junit is working fine",str);
-	
-	
-	}
-
 	// p. 4  -- test will fail and have compile errors
 	// no Dollar class, no Constructor, no method, no fields
 	@Test
@@ -20,7 +12,6 @@ public class MainTest {
 		five.times(2);
 		assertEquals(10, five.amount);
 	}
-	
 	
 	
 }
